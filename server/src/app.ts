f4e6eb51e91285f5f2c2b0a1a1fb1461aaaf847e
Gül Sevim Bülbül <gul.bulbul@dyorex.com>
2026-05-07 14:52:35 +0300

    Prepare portfolio for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
